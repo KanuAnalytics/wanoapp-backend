343513f187a617b9a69a4f2d9205230473885585
Nasir <nasir.khalid.new@gmail.com>
2025-08-18 03:24:48 +0500

    added thumbnail for video

--- app/graphql/mutations_folder/video_editor.py
+++ app/graphql/mutations_folder/video_editor.py
@@ -52,7 +52,8 @@ async def call_video_service_api(input: CompileVideoInput, user_id: str) -> Dict
                 "ratio": input.ratio,
                 "videoType": input.videoType,
                 "description": input.description,
-                "userId": user_id  # Add the user_id field
+                "userId": user_id,  # Add the user_id field
+                "thumbnail" : input.thumbnail
             }
         }
         
